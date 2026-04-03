26157e9b20fd3dfafcdc88084ded8de4a401b3a2
Shreyas262 <shreyasmahajan2602@gmail.com>
2026-04-03 23:25:14 +0530

    Sections added to show the indredients and recipe.

--- Chef_Claude/src/index.jsx
+++ Chef_Claude/src/index.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './Header'
-import Main from './Main'
+import Main from './main'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
